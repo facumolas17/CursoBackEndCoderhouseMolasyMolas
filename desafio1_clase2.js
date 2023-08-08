6491d7b21ba81a6b30a3cf5dcc6775019dbd3e7c
Facu Molas <facundomolasymolas@gmail.com>
2023-08-08 17:45:15 +0200

    empezando el curso de backend

---
+++ desafio1_clase2.js
@@ -0,0 +1,71 @@
+
+class ProductManager{
+
+    constructor(){
+        this.products=[];
+    }
+
+    addProduct(title,description,price,thumbnail,code,stock){ 
+
+
+        let newCode = this.products.find((prod) => prod.code === code); //primera validación por código
+        if(newCode){
+            return console.log(`Ya existe un producto con el código ${code}`);
+        }
+
+        if(!title || !description || !price || !thumbnail || !code ||!stock ){//segunda validación por si hay algún dato mal ingresado
+            return console.error("Alguno de sus datos es incorrecto");
+        }
+        else{
+
+            let newProduct={
+                title,
+                description,
+                price,
+                thumbnail,
+                code,
+                stock
+            }
+
+            if(this.products.length===0){
+                newProduct.id=1;
+            }else{
+                newProduct.id=this.products[this.products.length -1].id + 1;    
+            }
+
+            this.products.push(newProduct);
+    }
+        
+        
+    }
+
+    getProducts(){
+        return this.products;
+    }
+
+    getProductById(id){
+        
+        let productFind= this.products.find((product) => product.id === id);
+
+        if(!productFind){
+            console.error(`No se encontró ningún producto con el id ${id}`);
+           
+        }else{
+
+            return productFind;
+        
+        }
+        
+    }
+}
+
+let pm=new ProductManager();
+
+pm.addProduct("queso1","jdakks  sfkgdm dk", 2300, "http://jkjnskdc.ad", "QH4562", 200);
+pm.addProduct("fiambre2","jdakks gdh  sfkgdm dk", 2700, "http://jkjnskdfsc.ad", "FF4562", 150);
+pm.addProduct("fiambre3","jdakks gdh  sfkgdm dk", 2700, "http://jkjnskdfsc.ad", "FF452", 150);
+
+console.log(pm.getProducts());
+console.log("----------------");
+console.log(pm.getProductById(2));
+console.log(pm.getProductById(8));
